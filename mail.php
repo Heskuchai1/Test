9de56019d6d6297615bd6f1b7eaf312372c6e592
Heskuchai1 <128410742+Heskuchai1@users.noreply.github.com>
2023-03-20 20:22:48 +0200

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,5 +1,12 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
 require_once('phpmailer/PHPMailerAutoLoad.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
@@ -9,14 +16,14 @@ $password2 = $_POST['password2'];
 
 
 $mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
+$mail->Host = 'sandbox.smtp.mailtrap.io';
 $mail->SMTPAuth = true;
-$mail->UserName = 'otprava02@gmail.com';
-$mail->Password = '@204%365D00-i##m*%$$a';
+$mail->UserName = 'f57a14450f5056';
+$mail->Password = 'ae1081c260925b';
 $mail->SMTPSecure = 'ssl';
-$mail->Port = 465;
+$mail->Port = 2525;
 
-$mail->setFrom('otprava02@gmail.com');
+$mail->setFrom('sandbox.smtp.mailtrap.io');
 $mail->addAddress('dimdimih356@gmail.com');
 
 $mail->isHTML(true);
